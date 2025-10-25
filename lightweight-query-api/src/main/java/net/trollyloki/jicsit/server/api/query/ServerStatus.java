cbbbcf9c645a739e1523ebe17e4938a2f32c93a6
TrollyLoki <trollyloki@gmail.com>
2025-10-24 23:22:47 -0400

    Add method to allow checking if the HTTPS API is available in a particular server state

--- lightweight-query-api/src/main/java/net/trollyloki/jicsit/server/api/query/ServerStatus.java
+++ lightweight-query-api/src/main/java/net/trollyloki/jicsit/server/api/query/ServerStatus.java
@@ -9,23 +9,38 @@ public enum ServerStatus {
      * The server is offline.
      * Servers will never send this as a response.
      */
-    OFFLINE,
+    OFFLINE(false),
 
     /**
      * The server is running, but no save is currently loaded.
      */
-    IDLE,
+    IDLE(true),
 
     /**
      * The server is currently loading a map.
      * In this state, the HTTPS API is unavailable.
      */
-    LOADING,
+    LOADING(false),
 
     /**
      * The server is running, and a save is loaded.
      * The server is joinable by players.
      */
-    PLAYING
+    PLAYING(true);
+
+    private final boolean httpsApiAvailable;
+
+    ServerStatus(boolean httpsApiAvailable) {
+        this.httpsApiAvailable = httpsApiAvailable;
+    }
+
+    /**
+     * Checks if the HTTPS API is available while the server is in this state.
+     *
+     * @return {@code true} if the HTTPS API is available, or {@code false} if it is unavailable
+     */
+    public boolean isHttpsApiAvailable() {
+        return httpsApiAvailable;
+    }
 
 }
