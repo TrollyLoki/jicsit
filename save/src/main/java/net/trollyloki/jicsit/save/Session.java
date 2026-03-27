29645209aea832e6107ef12be57e62cf73744f73
TrollyLoki <trollyloki@gmail.com>
2026-03-26 22:22:41 -0400

    Add a method to find a specific SaveHeader in a Session

--- save/src/main/java/net/trollyloki/jicsit/save/Session.java
+++ save/src/main/java/net/trollyloki/jicsit/save/Session.java
@@ -1,6 +1,7 @@
 package net.trollyloki.jicsit.save;
 
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 import java.util.List;
 
@@ -12,5 +13,20 @@ import java.util.List;
  */
 @NullMarked
 public record Session(String sessionName, List<SaveHeader> saveHeaders) {
+
+    /**
+     * Finds a specific header in the list of saves belonging to this session.
+     *
+     * @param saveName name of the save (without any file extension)
+     * @return save header, or {@code null} if not found
+     */
+    public @Nullable SaveHeader find(String saveName) {
+        for (SaveHeader header : saveHeaders) {
+            if (header.saveName().equals(saveName)) {
+                return header;
+            }
+        }
+        return null;
+    }
 
 }
